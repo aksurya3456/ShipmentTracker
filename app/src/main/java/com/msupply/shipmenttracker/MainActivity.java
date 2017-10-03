fc9fe546f180b5a5010a4a09888d1b4ce42e72ea
aksurya3456 <aksurya3456@gmail.com>
2017-10-03 18:48:01 +0530

    03 Oct 2017

--- app/src/main/java/com/msupply/shipmenttracker/MainActivity.java
+++ app/src/main/java/com/msupply/shipmenttracker/MainActivity.java
@@ -2,6 +2,8 @@ package com.msupply.shipmenttracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v)
+    {
+        EditText editText =(EditText) findViewById(R.id.phone);
+        editText.setText("Button Clicked");
     }
 }
