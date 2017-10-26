d9bf23772962f31778afc80236a2a65026df8a92
aksurya3456 <aksurya3456@gmail.com>
2017-10-26 17:49:19 +0530

    Shipment details page added with dummy text

--- app/src/main/java/com/msupply/shipmenttracker/ShipmentDetails.java
+++ app/src/main/java/com/msupply/shipmenttracker/ShipmentDetails.java
@@ -2,16 +2,28 @@ package com.msupply.shipmenttracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class ShipmentDetails extends AppCompatActivity {
 
     private SessionManager sessionManager;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shipment_details);
+
+        /*toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });*/
 
         sessionManager = new SessionManager(this);
 
@@ -19,5 +31,20 @@ public class ShipmentDetails extends AppCompatActivity {
         info_text.setText(sessionManager.getKEY_SHIPMENT_ID()/* +
         " " + sessionManager.getKEY_PICKUP_DATE() + " " +
         sessionManager.getKEY_DELIVERY_DATE()*/);
+    }
+
+
+    public void goBack(View view) {
+        onBackPressed();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
     }
 }
