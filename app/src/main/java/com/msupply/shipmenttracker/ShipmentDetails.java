215aaf54f477d131eaaddff340c54d2fd50b7c67
aksurya3456 <aksurya3456@gmail.com>
2017-10-28 15:09:55 +0530

    Dummy Tracking Page Added

--- app/src/main/java/com/msupply/shipmenttracker/ShipmentDetails.java
+++ app/src/main/java/com/msupply/shipmenttracker/ShipmentDetails.java
@@ -1,11 +1,15 @@
 package com.msupply.shipmenttracker;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShipmentDetails extends AppCompatActivity {
 
@@ -38,6 +42,40 @@ public class ShipmentDetails extends AppCompatActivity {
         onBackPressed();
     }
 
+    public void agreeButtonClick(View view) {
+        //Toast.makeText(this,"Agree Button Clicked", Toast.LENGTH_SHORT).show();
+
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int choice) {
+                switch (choice) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        positiveButtonClickListen();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        negativeButtonClickListen();
+                }
+
+            }
+        };
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage(R.string.warning_message_before_start_tracking)
+                .setCancelable(false)
+                .setPositiveButton(R.string.shipment_details_page_yes_button_text, dialogClickListener)
+                .setNegativeButton(R.string.shipment_details_page_no_button_text, dialogClickListener)
+                .show();
+    }
+
+    public void positiveButtonClickListen() {
+        Toast.makeText(this, R.string.shipment_details_page_yes_button_clicked, Toast.LENGTH_SHORT).show();
+        Intent trackingActivityIntent = new Intent(this, TrackingDetails.class);
+        startActivity(trackingActivityIntent);
+    }
+
+    public void negativeButtonClickListen() {
+        Toast.makeText(this, R.string.shipment_details_page_no_button_clicked, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/com/msupply/shipmenttracker/TrackingDetails.java
@@ -0,0 +1,13 @@
+package com.msupply.shipmenttracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TrackingDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracking_details);
+    }
+}
